749319ac8ee947ddaca4881995fe9bd87f9ef3c9
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-09-02 15:01:05 +0200

    added base project

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const nunjucks = require("nunjucks");
+
+const app = express();
+
+// middle wares
+app.use(morgan("short"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+nunjucks.configure('views', {
+    autoescape:true,
+    express:app
+})
+
+
+app.get("/", (req, res)=>{
+    res.send("hello there");
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=>{
+    console.log("app starting on http://localhost:3000");
+});
