6338939c1d4aa8bc87486f60175bac204fe37832
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-09-03 22:03:37 +0200

    login form names correction

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,21 @@
-
 const express = require("express");
 
 const router = express.Router();
+
+// models
+var User = require("../models/user");
 
 router.get("/login", (req, res) => {
     res.render("login.html");
 });
 
 router.post("/login", (req, res) => {
-    res.send("login not implemented");
+    // validate user
+    let username = req.body.username;
+    let password = req.body.password;
+
+    console.log(req.body);
+    res.redirect("/login");
 });
 
 router.get("/signup", (req, res) => {
