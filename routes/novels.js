4520c210f5ded10bc20bd79e3942b474971c0fee
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-09-19 15:14:13 +0200

    novels page added

--- routes/novels.js
+++ routes/novels.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const Novel = require("../models/novel")
+
 router.get("/novels/:novelid", (req, res, next) => {
-    res.send(`not implemented yet ${req.params.novelid}`);
+    Novel.findOne({novelid : req.params.novelid}, (err, novel) => {
+        if (err)
+            return next(err);
+        if (!novel)
+            return next();
+        
+        res.render("novel.html", {novel : novel});
+    });
 })
 
 module.exports = router;
