1bd308860add0084c07e810a219748ae6d8980d6
tejian <sabingurung68@gmail.com>
2019-10-07 22:01:16 +0100

    toggle summary implemented

--- public/js/novel.js
+++ public/js/novel.js
@@ -1,5 +1,25 @@
 
-$(document).ready(()=>{
+function showSummary(){
+    $(".show-summary-btn").hide();
+    $(".hide-summary-btn").show();
+    $(".summary-section > p").show();
+}
 
+function hideSummary(){
+    $(".hide-summary-btn").hide();
+    $(".summary-section > p").hide();
+    $(".show-summary-btn").show();
+}
+
+$(document).ready(()=>{
     console.log("hello world");
+    $(".show-summary-btn").click(showSummary);
+    $(".hide-summary-btn").click(hideSummary);
+
+    $(".hide-summary-btn").hide();
+    $(".summary-section > p").hide();
+
+    $(".edit-button").click(()=>{alert("Not implemented yet");});
+
+
 });
