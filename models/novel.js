369f8c7dd758788ec5868ec621e15e81f0fc3785
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-10-10 14:53:20 +0200

    novel counter fix

--- models/novel.js
+++ models/novel.js
@@ -2,6 +2,13 @@
 const mongoose = require("mongoose")
 const User = require("../models/user")
 
+const novelUniqueIdSchema = mongoose.Schema({
+    curId :  {type : Number},
+    name : {type : String}
+});
+
+const NovelUniqueId = mongoose.model("NovelUniqueId", novelUniqueIdSchema);
+
 var novelSchema = mongoose.Schema({
     novelid : {type : Number},
     author : {type : String, required: true},
@@ -13,19 +20,26 @@ var novelSchema = mongoose.Schema({
 });
 
 novelSchema.pre("save", function(cb){
-    User.findOne({username : this.username}, (err, user)=>{
+    User.findOne({username : this.author}, (err, user)=>{
         if (err){
             return cb(err);
         }
-        else if(user){
+        else if(user == null){
             return cb(new Error(`${this.username} does not exist`));
         }
+
         this.updatedAt = Date.now();
-        Novel.countDocuments((err, c)=>{
-            if (err) return cb(err);
-            this.novelid = c + 1;
+        if (this.isNew){
+            NovelUniqueId.findOne({name: "novel"}, (err, novelUid)=>{
+                this.novelid = novelUid.curId;
+                novelUid.curId = novelUid.curId+1;
+                novelUid.save();
+                cb();
+            })
+        }
+        else{
             cb();
-        })
+        }
     });
 });
 
